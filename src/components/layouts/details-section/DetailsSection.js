fb69791f1b2d228d89131d91d4ef7722f414890a
greentealatte2105 <20521775@ms.uit.edu.vn>
2023-12-26 15:17:22 +0700

    add rating star and comments

--- src/components/layouts/details-section/DetailsSection.js
+++ src/components/layouts/details-section/DetailsSection.js
@@ -4,16 +4,18 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { BookData } from '../../../util/BookData';
 import ProductListingCard from '../../cards/product-listing-card/ProductListingCard';
 import { UserContext, CartContext } from '../../../App';
+import { FaStar } from 'react-icons/fa';
+import useNode from '../../../hooks/useNode';
+import Comment from '../comment/comment';
 
 const DetailsSection = () => {
     const { id } = useParams();
     const [bookData, setBookData] = useState({});
-
+    const [rating, setRating] = useState(null);
     const user = useContext(UserContext);
     const {cartItems, setCartItems} = useContext(CartContext);
-
+    const [hover, setHover] = useState(null);
     const navigate = useNavigate();
-    
     useEffect(() => {
         let newData = BookData.filter((book) => book.id === parseInt(id));
         setBookData(newData[0])
@@ -29,7 +31,29 @@ const DetailsSection = () => {
             alert("Please Login or Sign up first..");
         }
     }
+    const comments = {
+        id: 1,
+        items: []
+    }
+    const [commentsData, setCommentsData] = useState(comments);
 
+    const { insertNode, editNode, deleteNode } = useNode();
+
+    const handleInsertNode = (folderId, item) => {
+        const finalStructure = insertNode(commentsData, folderId, item);
+        setCommentsData(finalStructure);
+    };
+
+    const handleEditNode = (folderId, value) => {
+        const finalStructure = editNode(commentsData, folderId, value);
+        setCommentsData(finalStructure);
+    };
+
+    const handleDeleteNode = (folderId) => {
+        const finalStructure = deleteNode(commentsData, folderId);
+        const temp = { ...finalStructure };
+        setCommentsData(temp);
+    };
     return (
         <section className="detail-section-container">
             <div className='container'>
@@ -50,6 +74,34 @@ const DetailsSection = () => {
                         <a onClick={handleAddToCart} className="button-primary">Add To Cart</a>
                     </div>
                 </div>
+                <h3>Rating and Reviews</h3>
+                {[...Array(5)].map( (star, index) => {
+                    const currentRating = index + 1;
+                    return (
+                        <label>
+                            <input
+                                type='radio'
+                                name='rating'
+                                value={currentRating}
+                                onClick={() => setRating(currentRating)}
+                            />
+                            <FaStar 
+                                className='star' 
+                                size={20}
+                                color={currentRating <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                                onMouseEnter={() => setHover(currentRating)}
+                                onMouseLeave={() => setHover(null)}
+                            />
+                        </label>
+                    );
+                })}
+                <Comment
+                    handleInsertNode={handleInsertNode}
+                    handleEditNode={handleEditNode}
+                    handleDeleteNode={handleDeleteNode}
+                    comment={commentsData}
+                    user={user}
+                />
                 <h3>Others</h3>
                     <div className="listing-container">
                         {BookData.slice(0,4).map((book) => (
